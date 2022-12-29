bfafbf710765c314124c56c924e33c821335f11f
masachika <masachika.kmd@gmail.com>
2022-12-29 16:40:11 +0900

    相対パスを絶対パスに変換

--- parameters/data_path.py
+++ parameters/data_path.py
@@ -1,5 +1,7 @@
-DIR_INPUT = "data/input"
+import os
+
+DIR_INPUT = os.path.abspath("data/input")
 DIR_EDF = f"{DIR_INPUT}/edf_data"
-DIR_ARRAYS = "data/arrays"
-DIR_OUTPUT = "data/output"
-DIR_PROCESSED = "data/processed"
+DIR_ARRAYS = os.path.abspath("data/arrays")
+DIR_OUTPUT = os.path.abspath("data/output")
+DIR_PROCESSED = os.path.abspath("data/processed")
