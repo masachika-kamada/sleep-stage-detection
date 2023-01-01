ed5c6e818ab1c750b7fbfd279924891a265645a8
masachika <masachika.kmd@gmail.com>
2023-01-01 18:33:00 +0900

    accuracyの導出を追加

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,6 @@
-from sklearn.metrics import roc_auc_score
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 
 def AUC(y_true, y_pred, onehot=False):
@@ -14,3 +16,18 @@ def AUC(y_true, y_pred, onehot=False):
         else:
             auc = roc_auc_score(y_true, y_pred, multi_class="ovr")
     return auc
+
+
+def get_acc(y_true, y_pred):
+    y_pred = np.argmax(y_pred, axis=1)
+    acc = accuracy_score(y_true, y_pred)
+    return acc
+
+
+if __name__ == "__main__":
+    fname = "oof_ch7_230101_fmore.csv"
+    df = pd.read_csv(f"data/output/{fname}")
+    y_true = df["label"].values
+    y_pred = df[["pred_0", "pred_1", "pred_2", "pred_3", "pred_4"]].values
+    print(y_true.shape, y_pred.shape)
+    print(get_acc(y_true, y_pred))
